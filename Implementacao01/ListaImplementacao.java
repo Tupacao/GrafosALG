ad2dd1d926af27d81109e24da79650aff6f6f5a3
Tupacao <arthur12268@gmail.com>
2024-08-29 16:25:21 -0300

    Fim lista implementação

--- Implementacao01/ListaImplementacao.java
+++ Implementacao01/ListaImplementacao.java
@@ -2,9 +2,9 @@ package Implementacao01;
 
 // Implementação da lista de adjacência vista nos slides da disciplina.
 // A escolha foi devido a maior facilidade de implementação e pela menor utilização de memória se comparado com os outros algoritmos estudados
-// Optei por velocidade e portanto criei dois vetores, um com os sucessores e outro com os predecessores
 
-import java.io.RandomAccessFile;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -30,15 +30,6 @@ class NodeD {
         grau++;
     }
 
-    public boolean isAntecessor (int vertice){
-        boolean resp = true;
-        if(list.indexOf(vertice) == -1){
-            resp = false;
-        }
-
-        return resp;
-    }
-
 }
 
 public class ListaImplementacao {
@@ -58,50 +49,38 @@ public class ListaImplementacao {
         System.out.print("Insira o nome do arquivo: ");
         String arq = sc.nextLine();
         System.out.print("Vértice a ser analisado: ");
-        int usefull = sc.nextInt();
+        int verticeEntrada = sc.nextInt();
 
         try {
+            
+            BufferedReader bf = new BufferedReader(new FileReader("./" + arq));
 
-            RandomAccessFile raf = new RandomAccessFile("./" + arq, "r");
-
-            temp = raf.readLine().split(" ");
+            temp = bf.readLine().split(" ");
             int tam = Integer.parseInt(temp[0]);
             sucessores = new NodeD[tam];
             antecessores = new NodeD[tam];
 
+
             preencherVetor(sucessores);
             preencherVetor(antecessores);
 
-            long start = System.nanoTime();
 
-            System.out.println("\nprocessando ...\n");
-
-            while ((aux = raf.readLine()) != null) {
+            while ((aux = bf.readLine()) != null) {
                 temp = aux.trim().split("\\s+");
                 sucessores[Integer.parseInt(temp[0]) - 1].add(Integer.parseInt(temp[1]));
                 antecessores[Integer.parseInt(temp[1]) - 1].add(Integer.parseInt(temp[0]));
             }
 
-            // LinkedList<Integer> numbers = new LinkedList<Integer>();
+            System.out.println("\nVértice " + verticeEntrada
+            + "\nGrau de saída: "
+            + sucessores[verticeEntrada - 1].getGrau()
+            + " - Sucessores " + sucessores[verticeEntrada - 1].getList()
+            + "\nGrau de entrada: "
+            + antecessores[verticeEntrada - 1].getGrau()
+            + " - Antecessores " +  antecessores[verticeEntrada - 1].getList() + "\n");
 
-            // for (int i = 0; i < sucessores.length; i++) {
-            //     if (sucessores[i].isAntecessor(usefull)) {
-            //         numbers.add(i+1);
-            //     }
-            // }
-
-
-            System.out.println("Vértice " + usefull + " - Sucessores: " + sucessores[usefull - 1].getGrau() + " "
-                    + sucessores[usefull - 1].getList() + " - Antecessores: " + antecessores[usefull - 1].getGrau()
-                    + " " +  antecessores[usefull - 1].getList()
-                     + "\n");
-
-                     long fim = System.nanoTime() - start;
-                     System.out.println(fim/1000000000);
-
-            raf.close();
+            bf.close();
             sc.close();
-
 
         } catch (
 
